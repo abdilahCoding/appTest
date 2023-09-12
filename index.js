5c44fc35a8a8838802370ed68e9f32b3a6a0a60b
abdilah Elaidi <abdilahelaidii@gmail.com>
2023-09-12 17:25:10 +0200

    new page

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 // Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, Express! nice');
+  res.send('Hello world, Express! nice');
 });
 
 // Start the server
